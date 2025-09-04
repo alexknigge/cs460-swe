edaa9b378b1a7653bed5371e3cf98451da65bc6d
Alex Knigge <aknigge1@unm.edu>
2025-09-03 22:22:29 -0600

    working demo (graphics are a bit scuffed)

--- GasPump/src/Main/Main.java
+++ GasPump/src/Main/Main.java
@@ -4,6 +4,7 @@ import Server.CommPort;
 import Server.Message;
 
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.concurrent.TimeUnit;
 
 // Main.Main
@@ -12,49 +13,62 @@ public class Main {
         CommPort mainToScreen = new CommPort("MainToScreen");
         //CommPort mainToPump = new CommPort("MainToPump");
         CommPort mainToFlowMeter = new CommPort("MainToFlowMeter");
-        //CommPort mainToCardReader = new CommPort("MainToCardReader");
+        CommPort mainToCardReader = new CommPort("MainToCardReader");
 
-        String initialScreenMessage = "t:01/s:3/f:2/c:0/Welcome!;" +
-                "t:2/s:2/f:1/c:0/Select Grade;" +
-                "t:4/s:2/f:1/c:3/87 Octane;$4.49;b:4/m;" +
-                "t:5/s:2/f:1/c:4/91 Octane;$4.99;b:5/m;" +
-                "t:6/s:2/f:1/c:1/93//";
-
-        Message screenUpdate = new Message(initialScreenMessage);
-        mainToScreen.send(screenUpdate);
-
-        System.out.println("[SERVER] Waiting...");
-        TimeUnit.SECONDS.sleep(5);
-        System.out.println("[SERVER] Starting up flow meter...");
-
-        mainToFlowMeter.send(new Message("CMD:START"));
-        TimeUnit.SECONDS.sleep(2);
-        Message flowMeter = mainToFlowMeter.get();
         while (true) {
-            if (flowMeter != null) {
-                if (flowMeter.toString().contains("FLOW:STOP")) {
-                    break;
-                }
-            }
-            mainToScreen.send(flowMeter);
-            System.out.println(flowMeter);
-            flowMeter = mainToFlowMeter.get();
-        }
-        System.out.println(flowMeter);
-        System.out.println("Done pumping");
-
-
-        /*
-        if (mainToPump.get().equals(new Message("on"))) {
             String initialScreenMessage = "t:01/s:3/f:2/c:0/Welcome!;" +
                     "t:2/s:2/f:1/c:0/Select Grade;" +
                     "t:4/s:2/f:1/c:3/87 Octane;$4.49;b:4/m;" +
                     "t:5/s:2/f:1/c:4/91 Octane;$4.99;b:5/m;" +
-                    "t:6/s:2/f:1/c:1/93//";
+                    "t:6/s:2/f:1/c:1/93;b:6/m//";
 
             Message screenUpdate = new Message(initialScreenMessage);
             mainToScreen.send(screenUpdate);
+
+            System.out.println("[SERVER] Waiting for card...");
+            TimeUnit.SECONDS.sleep(5);
+
+            screenUpdate = mainToCardReader.get();
+            mainToScreen.send(screenUpdate);
+
+            TimeUnit.SECONDS.sleep(5);
+
+            screenUpdate = mainToCardReader.get();
+            mainToScreen.send(screenUpdate);
+
+
+            TimeUnit.SECONDS.sleep(5);
+            System.out.println("[SERVER] Pumping gas...");
+            //System.out.println("[SERVER] Starting up flow meter...");
+
+            mainToFlowMeter.send(new Message("CMD:START"));
+            TimeUnit.SECONDS.sleep(2);
+            Message flowMeter = mainToFlowMeter.get();
+            while (true) {
+                if (flowMeter != null) {
+                    if (flowMeter.toString().contains("FLOW:STOP")) {
+                        break;
+                    }
+                }
+                mainToScreen.send(flowMeter);
+                System.out.println(flowMeter);
+                flowMeter = mainToFlowMeter.get();
+            }
+            TimeUnit.SECONDS.sleep(2);
+            flowMeter = mainToFlowMeter.get();
+            mainToScreen.send(flowMeter);
+            System.out.println("Done pumping");
+            TimeUnit.SECONDS.sleep(5);
+
+            Message finalMessage = new Message("t:01/s:3/f:2/c:0/Thank You!;" +
+                    "t:2/s:2/f:1/c:0/Receipt sent.;" +
+                    "t:03/s:2/f:1/c:0/;" +
+                    "t:04/s:2/f:1/c:0/;" +
+                    "t:05/s:2/f:1/c:0/;" +
+                    "t:06/s:2/f:1/c:0//");
+            mainToScreen.send(finalMessage);
+            TimeUnit.SECONDS.sleep(5);
         }
-         */
+
     }
 }
--- GasPump/src/SmallDevices/CardReader.java
+++ GasPump/src/SmallDevices/CardReader.java
@@ -20,6 +20,8 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Author: Dustin Ferguson
  * </p>
@@ -44,13 +46,27 @@ public class CardReader extends Application {
     private static IOPort commManager;
     Rectangle outerRect;
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) throws InterruptedException {
+        // launch(args);
+        // TODO: Change this, basically just for demo purposes
+        commManager = new IOPort("cardReaderToMain");
+        while (true) {
+            String initCard = "t:01/s:3/f:2/c:0/Tap to Pay Now;" +
+                    "t:2/s:2/f:1/c:0/Cancel;" + "b:1/m;" +
+                    "t:3/s:2/f:1/c:0/Help;" + "b:2/m;" +
+                    "t:04/s:2/f:1/c:0/;" + "b:3/x;//";
+            commManager.send(new Message(initCard));
+            String paymentAccepted = "t:01/s:3/f:2/c:0/Payment Accepted;" +
+                    "t:2/s:2/f:1/c:0/Thank you!;" +
+                    "t:3/s:2/f:1/c:0/You may begin fueling now;" +
+                    "t:04/s:2/f:1/c:0/;" + "b:1/x;//";
+            commManager.send(new Message(paymentAccepted));
+            TimeUnit.SECONDS.sleep(8);
+        }
     }
 
     @Override
     public void start(Stage primaryStage) {
-        commManager = new IOPort("cardReaderToMain");
         BorderPane root = createUI();
         Scene scene = new Scene(root, 600, 300);
         primaryStage.setTitle("Card Reader UI Mockup");
