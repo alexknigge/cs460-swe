20479ad6d42a46eb68bf23a187f25f2fdfc62dc2
adrianfaust <adrianbfaust@gmail.com>
2025-09-04 15:12:28 -0600

    Add and use constants for deviceIDS

--- GasPump/src/Server/DeviceMapper.java
+++ GasPump/src/Server/DeviceMapper.java
@@ -3,56 +3,64 @@ package Server;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-// TODO: Rename vars and update javadoc comments
+
 /**
- * Maps port numbers to device names and provides configuration constants for the system.
+ * Maps device IDS to port numbers and provides configuration constants for the system.
  */
 public class DeviceMapper {
 
-    public static final String MAIN_HOST_NAME = "main host";
+    public static final String PUMP_TO_MAIN = "pumpToMain";
+    public static final String MAIN_TO_PUMP = "MainToPump";
 
-    /**
-     * The network address of the device server (for local testing).
-     */
-    public static final String DEVICE_SERVER_HOST = "localhost";
-    /**
-     * The designated port for the pump device, which will act as a server.
-     */
-    public static final int PUMP_SERVER_PORT = 12345;
+    public static final String CARD_READER_TO_MAIN = "cardReaderToMain";
+    public static final String MAIN_TO_CARD_READER = "MainToCardReader";
+
+    public static final String FLOW_METER_TO_MAIN = "flowMeterToMain";
+    public static final String MAIN_TO_FLOW_METER = "MainToFlowMeter";
+
+    public static final String SCREEN_TO_MAIN = "screenToMain";
+    public static final String MAIN_TO_SCREEN = "MainToScreen";
+
     private static final List<String> deviceClientIDs = Arrays.asList(
-            "pumpToMain",
-            "cardReaderToMain",
-            "flowMeterToMain",
-            "screenToMain");
+            PUMP_TO_MAIN,
+            CARD_READER_TO_MAIN,
+            FLOW_METER_TO_MAIN,
+            SCREEN_TO_MAIN);
     private static final List<String> mainServerIDs = Arrays.asList(
-            "MainToPump",
-            "MainToCardReader",
-            "MainToFlowMeter",
-            "MainToScreen");
-    public static boolean shouldIDBeAServer(String serverID) {
-        return mainServerIDs.contains(serverID);
-    }
-
+            MAIN_TO_PUMP,
+            MAIN_TO_CARD_READER,
+            MAIN_TO_FLOW_METER,
+            MAIN_TO_SCREEN);
     // A map of port numbers to their corresponding device names.
     private static final Map<String, Integer> idToPortMap = Map.of(
             // Device Mappings
-            "pumpToMain", 1234,
-            "cardReaderToMain", 1235,
-            "flowMeterToMain", 1236,
-            "screenToMain", 1237,
+            PUMP_TO_MAIN, 1234,
+            CARD_READER_TO_MAIN, 1235,
+            FLOW_METER_TO_MAIN, 1236,
+            SCREEN_TO_MAIN, 1237,
             // Main (Server) Mappings
-            "MainToPump", 1234,
-            "MainToCardReader", 1235,
-            "MainToFlowMeter", 1236,
-            "MainToScreen", 1237
+            MAIN_TO_PUMP, 1234,
+            MAIN_TO_CARD_READER, 1235,
+            MAIN_TO_FLOW_METER, 1236,
+            MAIN_TO_SCREEN, 1237
     );
 
     /**
-     * Returns the device name for a given port.
+     * Returns whether a device should be a server.
      *
-     * @param deviceID the port number
-     * @return the device name as a String
-     * @throws IllegalArgumentException if the port is not mapped to any device.
+     * @param deviceID The ID of the device
+     * @return True if the device should be a server, false if it should be a client (or the deviceID does not exist)
+     */
+    public static boolean shouldIDBeAServer(String deviceID) {
+        return mainServerIDs.contains(deviceID);
+    }
+
+    /**
+     * Returns a port for a given deviceID.
+     *
+     * @param deviceID The string ID of the device (defined as constants in DeviceMapper)
+     * @return the port number.
+     * @throws IllegalArgumentException if the deviceID is not mapped to any port.
      */
     public static int getDevicePort(String deviceID) {
         if (!idToPortMap.containsKey(deviceID)) {
