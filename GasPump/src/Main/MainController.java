72d955e2b5906d127cbbcc3b9afc060e857ed06e
Alex Knigge <aknigge1@unm.edu>
2025-09-23 16:12:51 -0600

    blank development file for demo stuff

---
+++ GasPump/src/Main/MainController.java
@@ -0,0 +1,25 @@
+package Main;
+/*
+ Instantiates all objects and starts while loop for
+ pumping service
+ */
+
+public class MainController {
+    /*
+    TODO: start up objects with ioport comms
+     */
+    private void startUp() {
+
+    }
+
+    /*
+    TODO: add objects incrementally as they work
+     */
+    private void run() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
